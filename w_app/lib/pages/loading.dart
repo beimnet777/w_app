6a625988167784fbee71406116589dd1601a5eba
Beimnet Guta <beimnetbekele123@gmail.com>
2022-12-23 09:19:18 +0300

    http request

--- w_app/lib/pages/loading.dart
+++ w_app/lib/pages/loading.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -13,7 +14,10 @@ class _LoadingState extends State<Loading> {
   Future getData() async {
     String name =
         await Future.delayed(const Duration(seconds: 2), (() => "beimnet"));
+    Response data =
+        await get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
     print(name);
+    print(jsonDecode(data.body));
   }
 
   @override
